bd2590463f5219ff6a7dc82a417a5e9c8b6a067d
jeph.dev <jeph.dev@icloud.com>
2026-01-05 09:42:23 +0800

    Add support for CADOPE file processing and update year in employee report naming

--- main.py
+++ main.py
@@ -6,16 +6,29 @@ from helper.produce_timesheets import copy_gsheets
 load_dotenv()
 FILE_ID = os.getenv('FILE_ID')
 FOLDER_ID = os.getenv('FOLDER_ID')
+CADOPE_FILE_ID = os.getenv('CADOPE_FILE_ID')
 
 def main():
 
     employee_list_path = "employee_list.txt"
+    cadope_list_path = "cad_ope_list.txt"
+
     try:
         with open(employee_list_path, "r") as employees:
             for employee in employees:
                 copy_gsheets(FILE_ID, 
                              FOLDER_ID,
-                             f"2025作業日報_{employee}")
+                             f"2026作業日報_{employee}")
+
+    except Exception as e:
+        print(f'An error occurred: {e}')  
+
+    try:
+        with open(cadope_list_path, "r") as cadopes:
+            for cadope in cadopes:
+                copy_gsheets(CADOPE_FILE_ID, 
+                             FOLDER_ID,
+                             f"2026作業日報_{cadope}")
 
     except Exception as e:
         print(f'An error occurred: {e}')  
